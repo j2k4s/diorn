efc61e8e9ab8bd15aab736772bce0583eab11048
j2k4s <joycekellydasilva17@gmail.com>
2022-06-27 21:54:42 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,117 @@
+import React from 'react';
+import {
+  View,
+  Image,
+  StyleSheet,
+  SafeAreaView,
+  StatusBar,
+  Text,
+  Pressable,
+  Linking,
+} from 'react-native';
+
+const colorGithub = '#010409';
+const colorFontGithub = '#C9D1D9';
+const colorDarkFontGithub = '#4F565E';
+
+const imageProfileGithub =
+  'https://avatars.githubusercontent.com/u/89051826?v=4';
+const urlToMyGithub = 'https://github.com/j2k4s';
+
+const App = () => {
+  const handlePressGoToGithub = async () => {
+    console.log('Verificando link');
+    const res = await Linking.canOpenURL(urlToMyGithub);
+    if (res) {
+      console.log('Link aprovado');
+      console.log('Abrindo link....');
+      await Linking.openURL(urlToMyGithub);
+    }
+  };
+
+  return (
+    <SafeAreaView style={style.container}>
+      <StatusBar backgroundColor={colorGithub} barStyle="light-content" />
+      <View style={style.content}>
+        <Image
+          accessibilityLabel="Joyce com chapeu mexicano fazendo hag lose"
+          style={style.avatar}
+          source={{uri: imageProfileGithub}}
+        />
+        <Text
+          accessibilityLabel="Nome: Joyce Kelly"
+          style={[style.defaultText, style.name]}>
+          Joyce Kelly
+        </Text>
+        <Text
+          accessibilityLabel="Nickname: j2k4s"
+          style={[style.defaultText, style.nickname]}>
+          j2k4s
+        </Text>
+        <Text
+          accessibilityLabel="Descrição: Estudante de Ciência da Computação | Iniciando na carreira de TI"
+          style={[style.defaultText, style.description]}>
+          Estudante de Ciência da Computação | Iniciando na carreira de TI
+        </Text>
+
+        <Pressable onPress={handlePressGoToGithub}>
+          <View style={style.button}>
+            <Text style={[style.defaultText, style.textButton]}>
+              Open in Github
+            </Text>
+          </View>
+        </Pressable>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
+
+const style = StyleSheet.create({
+  container: {
+    // Column
+    backgroundColor: colorGithub,
+    flex: 1, // Expandir para a tela inteira
+    justifyContent: 'center',
+    alignItems: 'center',
+    // flexDirection: 'row',
+  },
+  content: {
+    alignItems: 'center',
+    padding: 20,
+  },
+  avatar: {
+    height: 200,
+    width: 200,
+    borderRadius: 100,
+    borderColor: 'white',
+    borderWidth: 2,
+  },
+  defaultText: {
+    color: colorFontGithub,
+  },
+  name: {
+    marginTop: 20,
+    fontWeight: 'bold',
+    fontSize: 24,
+  },
+  nickname: {
+    fontSize: 18,
+    color: colorDarkFontGithub,
+  },
+  description: {
+    fontWeight: 'bold',
+    fontSize: 14,
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: colorDarkFontGithub,
+    borderRadius: 10,
+    padding: 20,
+  },
+  textButton: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
